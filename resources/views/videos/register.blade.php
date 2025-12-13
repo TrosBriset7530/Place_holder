121479ea16d718606306d7896590af230490367a
TrosBriset7530 <trosbriset@gmail.com>
2025-12-13 17:45:49 +0700

    Login & register style

--- resources/views/videos/register.blade.php
+++ resources/views/videos/register.blade.php
@@ -8,13 +8,17 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    
 </head>
-<body class="bg-light">
+<body class="bg-black">
 
-    <div class="container d-flex justify-content-center align-items-center" style="height: 100vh;">
-        <div class="card shadow p-4" style="width: 400px;">
-
-            <h3 class="text-center mb-3">Register</h3>
+    <div class="container d-flex justify-content-center align-items-center" style="height: 100vh; padding: 0">
+        <div class="card shadow" style="width: 40vh; border-radius:  25px 25px 15px 15px; border: none;">
+            <div class ="card-header mb-4" style="background-color: #5a1616; border-radius: 20px 20px 0 0; text-align: center; 
+            color: white; width:100%; padding: 15px; border-color: #5a1616;  border-width: 5px; border-style: solid;">
+                <h4 class="mb-0">Register Aplikasi</h4>
+            </div>
+            {{-- <h3 class="text-center mb-3">Register</h3> --}}
 
             <!-- Success Message -->
             @if(session('success'))
@@ -37,7 +41,7 @@
 
             <form action="{{ route('register') }}" method="POST">
                 @csrf
-
+        <div style = "width: 80%; margin-left: auto; margin-right: auto;">
                 <div class="mb-3">
                     <label for="name" class="form-label">Name</label>
                     <input type="text" class="form-control" id="name" name="name" required>
@@ -53,10 +57,11 @@
                     <input type="password" class="form-control" id="password" name="password" required>
                 </div>
 
-                <button class="btn btn-primary w-100" type="submit">Register</button>
+                <button class="btn btn-primary w-100" type="submit" style="background-color: #5a1616">Register</button>
             </form>
+        </div>
 
-            <div class="mt-3 text-center">
+            <div class="mt-3 text-center" style="padding-bottom:20px">
                     <a href="{{ route('login') }}">Login</a>
             </div>
 
